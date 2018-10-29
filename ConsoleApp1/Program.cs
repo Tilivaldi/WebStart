21b8de651a6c24ae532db26e8fcfb16a59e94c62
Tima <tilvaldi@mail.ru>
2018-10-29 19:23:21 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[10];
+            int buff;
+            createArr arr2 = new createArr();
+            arr2.createArray(arr, 10);
+            arr2.PrintArray(arr);
+           
+
+            for(int i=0;i<9;i++)
+                for(int j = i + 1; j < 10; j++)
+                    if (arr[i] > arr[j])
+                    {
+                        buff = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = buff;
+                    }
+            Console.WriteLine();
+            arr2.PrintArray(arr);
+
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("Вставки");
+            Console.WriteLine();
+            int[] array = new int[10];
+            arr2.createArray(array, 10);
+            arr2.PrintArray(array);
+            Console.WriteLine();
+            for (int i=1; i<10;i++)
+            {
+                int new_end = array[i];
+                int j = i;
+                while (j>0 && new_end < array[j - 1])
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+                array[j] = new_end;
+            }
+
+            arr2.PrintArray(array);
+
+
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("Слияние");
+            Console.WriteLine();
+            int[] arraymerge = new int[10];
+            arr2.createArray(arraymerge,10);            
+            arr2.PrintArray(arraymerge);
+            arr2.MergeSort(arraymerge);
+            Console.WriteLine(); Console.WriteLine();
+            arr2.PrintArray(arraymerge);
+
+
+            Console.ReadKey();
+        }
+
+        
+
+       
+
+        
+    }
+}
---
+++ ConsoleApp1/createArr.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class createArr
+    {
+
+        public void createArray(int[] arr, int arrLenght)
+        {
+            Random rand1 = new Random();
+            for (int i = 0; i < arrLenght; i++)
+            {
+                arr[i] = rand1.Next(10);
+            }
+
+        }
+
+        public void PrintArray(int[] arr)
+        {
+            foreach (int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+        }
+
+        public int[] MergeSort(int[] array)
+        {
+            if (array.Length > 1)
+            {
+                //массивы для хранения половинок входящего буфера
+                int[] left = new int[array.Length / 2];
+                //для проверки ошибки некорректного разбиения массива,
+                //в случае если длина непарное число
+                int[] right = new int[array.Length - left.Length];
+
+                //заполнение субмассивов данными из входящего массива
+                for (int i = 0; i < left.Length; i++)
+                {
+                    left[i] = array[i];
+                }
+                for (int i = 0; i < right.Length; i++)
+                {
+                    right[i] = array[left.Length + i];
+                }
+                //если длина субмассивов больше еденици,
+                //то мы повторно (рекурсивно) вызываем функцию разбиения массива
+                if (left.Length > 1)
+                    left = MergeSort(left);
+                if (right.Length > 1)
+                    right = MergeSort(right);
+                //сортировка слиянием половинок
+                array = Merge(left, right);
+            }
+            //возврат отсортированного массива
+            return array;
+        }
+
+
+        public int[] Merge(int[] left, int[] right)
+        {
+            int[] array = new int[left.Length + right.Length];
+            int i = 0;
+            int l = 0;
+            int r = 0;
+            for (int x=0; x < array.Length; x++)
+            {
+                if (r >= right.Length)
+                {
+                    array[x] = left[l];
+                    l++;
+                }
+                else if (l < left.Length && left[l] < right[r])
+                {
+                    array[x] = left[l];
+                    l++;
+                }
+                else
+                {
+                    array[x] = right[r];
+                    r++;
+                }
+            }
+            return array;
+        }
+    }
+}
